70a82d70655084406170fbffd9478f5b56f09ac4
saeedeh <saeedeh@tiseno.com>
2018-05-20 16:45:23 +0800

    first commit

---
+++ SearchMusicPlace/Model/Classes/PaginSearchManager/PaginSearchManagerProgressReply.swift
@@ -0,0 +1,15 @@
+//
+//  PaginSearchManagerProgressReply.swift
+//  SearchMusicPlace
+//
+//  Created by Saeedeh on 20/05/2018.
+//  Copyright © 2018 tiseno. All rights reserved.
+//
+
+import Foundation
+struct PaginSearchManagerProgressReply{
+    
+    let progress:Float
+    let searchQuery:String
+    
+}
